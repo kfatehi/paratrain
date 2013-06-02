d2cc182794b5ad79f4c93701587abf2549449493
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-06-01 22:32:40 -0700

    add recording code

--- Paratrain-iOS/Paratrain/PTParatrain.h
+++ Paratrain-iOS/Paratrain/PTParatrain.h
@@ -9,6 +9,26 @@
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
 
-@interface PTParatrain : AVAudioRecorder
-- (void) listen;
+@interface PTParatrain : NSObject {
+    AVAudioPlayer *audioPlayer;
+    AVAudioRecorder *audioRecorder;
+    int recordEncoding;
+    enum
+    {
+        ENC_AAC = 1,
+        ENC_ALAC = 2,
+        ENC_IMA4 = 3,
+        ENC_ILBC = 4,
+        ENC_ULAW = 5,
+        ENC_PCM = 6,
+    } encodingTypes;
+}
+
+- (NSURL*) tempFilePath;
+
+-(void) startRecording;
+-(void) stopRecording;
+-(void) playRecording;
+-(void) stopPlaying;
+
 @end
--- Paratrain-iOS/Paratrain/PTParatrain.m
+++ Paratrain-iOS/Paratrain/PTParatrain.m
@@ -7,13 +7,50 @@
 //
 
 #import "PTParatrain.h"
-#import <AVFoundation/AVFoundation.h>
 
 @implementation PTParatrain
 
 // Start capturing audio
 - (void) listen
 {
-    NSLog(@"Paratrain is creating a capture session");
+    NSLog(@"Paratrain#listen");
+    recordEncoding = ENC_IMA4;
+}
+
+- (void) startRecording
+{
+    NSLog(@"startRecording");
+    audioRecorder = nil;
+    
+    // Init audio with record capability
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setCategory:AVAudioSessionCategoryRecord error:nil];
+    
+    NSDictionary *recordSettings = @{
+       AVEncoderAudioQualityKey:@(AVAudioQualityHigh),
+       AVFormatIDKey:@(kAudioFormatAppleIMA4),
+       AVSampleRateKey:@16000.0,
+       AVNumberOfChannelsKey:@1
+    };
+    
+    NSURL *url = self.tempFilePath;
+    
+    NSError *error = nil;
+    audioRecorder = [[ AVAudioRecorder alloc] initWithURL:url settings:recordSettings error:&error];
+    
+    if ([audioRecorder prepareToRecord] == YES){
+        [audioRecorder record];
+    }else {
+        int errorCode = CFSwapInt32HostToBig ([error code]);
+        NSLog(@"Error: %@ [%4.4s])" , [error localizedDescription], (char*)&errorCode);
+        
+    }
+    NSLog(@"recording");
+}
+
+- (NSURL*) tempFilePath
+{
+    NSURL *tmpDirURL = [NSURL fileURLWithPath:NSTemporaryDirectory() isDirectory:YES];
+    return [[tmpDirURL URLByAppendingPathComponent:@"recording"] URLByAppendingPathExtension:@"ima4"];
 }
 @end
